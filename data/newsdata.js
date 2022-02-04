97b517e5bbfeadec3730221cc84ba58306225922
kp <kanhaiyapandit1997@gmail.com>
2022-02-04 16:29:13 +0530

    applied some changes in newsdata.js file

--- data/newsdata.js
+++ data/newsdata.js
@@ -21,14 +21,21 @@
 //         "slug"  :   "news1"
 //     },
 // ]
-
+var data=[];
 const NewsAPI = require('newsapi');
 const newsapi = new NewsAPI('46c1d49955ea492591cb268722003dd0');
+const a=newsapi.v2.topHeadlines({
+  country: 'us'
+})
+console.log(a)
 // To query /v2/top-headlines
 // All options passed to topHeadlines are optional, but you need to include at least one of them
-newsapi.v2.topHeadlines({
-  country: 'us'
-}).then(response => {
-    console.log(response)
-    module.exports=response
-});
+// newsapi.v2.topHeadlines({
+  // country: 'us'
+// }).then(response => {
+    // if (response.status == 'ok') {
+      // module.exports.data=response.status
+      // console.log('status ok')
+    // }
+// });
+// module.exports=news
